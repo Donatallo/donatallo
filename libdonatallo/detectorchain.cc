59f09b96ff4de45a9b91092a998998352a50f1f6
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-04-16 13:45:41 +0300

    Add Detector and DetectorChain implementations

---
+++ libdonatallo/detectorchain.cc
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2016 Dmitry Marakasov
+ *
+ * This file is part of donatallo.
+ *
+ * donatallo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * donatallo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with donatallo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libdonatallo/detector.hh>
+#include <libdonatallo/detectorchain.hh>
+
+namespace Donatallo {
+
+DetectorChain::DetectorChain() {
+}
+
+void DetectorChain::Prepare(StatusCallback&& status_callback) {
+	int n = 0;
+
+	if (status_callback)
+		status_callback(n, detectors_.size());
+
+	for (auto& detector : detectors_) {
+		detector->Prepare();
+
+		if (status_callback)
+			status_callback(n++, detectors_.size());
+	}
+}
+
+bool DetectorChain::Check(const Project& project) const {
+	for (const auto& detector : detectors_)
+		if (detector->Check(project))
+			return true;
+
+	return false;
+}
+
+}
